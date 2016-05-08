78c595ab916cd7c7a0bf8589b858eac96478e7d2
vesche <austinjackson892@gmail.com>
2016-05-08 15:57:43 -0400

    remove old rhel install script

--- install_rhel7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo yum -y install epel-release
-sudo yum -y install jq tcpdump
-read -p "Hostname or IP of remote Stenographer server? " HNAME
-sudo mkdir -p /etc/stenographer/certs
-cat ./config | sudo jq --arg v "$HNAME" '."Host"=$v' > /etc/stenographer/config
-sudo cp ./stenoread /usr/bin/
-sudo cp ./stenocurl /usr/bin/
