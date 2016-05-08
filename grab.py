be11d0e8412ad86a4892e4ff7945d62756f2c2a9
vesche <austinjackson892@gmail.com>
2016-05-08 19:39:09 -0400

    fix path to pcap

--- grab.py
+++ grab.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 print "Content-type: text/html\n\n"
-print "Grabbing your packets... Wait for this page to finish loading!"
-print "When your packets finish downloading they'll be <a href="./pcap/">here</a>."
+print "Grabbing your packets... Wait for this page to finish loading!<br>"
+print "When your packets finish downloading they'll be <a href=\"../pcap\">here</a>."
 
 import cgi, subprocess
 
